eb7e1fe7415a605c0d8128a327a517634ed46ac2
Erik <mortensenerikm@gmail.com>
2020-05-09 20:11:10 -0300

    Seeder y Factory para Product

--- app/Product.php
+++ app/Product.php
@@ -6,5 +6,14 @@ use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
-    //
+    
+    protected $fillable = [
+        'title',
+        'description',
+        'price',
+        'stock',
+        'status',
+    ];
+
+
 }
