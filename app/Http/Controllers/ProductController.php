0b8bf5c258be09637d5976cf187a65b65c0d5a7e
Erik <mortensenerikm@gmail.com>
2020-05-10 20:51:53 -0300

    Edit and Update Products

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -18,33 +18,45 @@ class ProductController extends Controller
         ]);
     }
 
-    public function store(){
-
-    }
-
     public function create(){
-        return 'This is the form to create product from CONTROLLER.';
+        return view('products.create');
     }
 
+    public function store(){
+        
+        $product = Product::create(request()->all());
+
+        return $product;
+    }
+
+    
     public function show($product){
-
-        $product = Product::findOrFail($product);
-
+        
         return view('products.show')->with([
-            'product' => $product,
+            'product' => Product::findOrFail($product),
+        ]);
+    }
+
+    public function edit($product){
+
+        return view('products.edit')->with([
+            'product' => Product::findOrFail($product),
         ]);
     }
 
     public function update($product){
+        $product = Product::findOrFail($product);
 
+        $product->update(request()->all());
+
+        return $product;
     }
+
     public function destroy($product){
 
     }
 
-    public function edit($product){
-        return "Showing the form to edit product with id {$product} from CONTROLLER.";
-    }
+    
 
 
 }
