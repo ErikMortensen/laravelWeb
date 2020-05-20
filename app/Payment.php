c4e52086c484395b58c08b8ec5f839122f1bbb0b
Erik <mortensenerikm@gmail.com>
2020-05-19 21:38:50 -0300

    Fillables, migration and factories

--- app/Payment.php
+++ app/Payment.php
@@ -6,5 +6,17 @@ use Illuminate\Database\Eloquent\Model;
 
 class Payment extends Model
 {
-    //
+    protected $fillable = [
+        'amount',
+        'payed_at',
+    ];
+
+    /**
+     * The attributes that should be mutated to dates.
+     *
+     * @var array
+     */
+    protected $dates = [ 
+        'payed_at' 
+    ];
 }
--- database/factories/ImageFactory.php
+++ database/factories/ImageFactory.php
@@ -6,7 +6,23 @@ use App\Image;
 use Faker\Generator as Faker;
 
 $factory->define(Image::class, function (Faker $faker) {
+
+    $fineName = $faker->numberBetween(1, 10) . '.jpg';
+
     return [
-        //
+        
+        'path' => public_path("img/products/{$filesName}"),
+
+    ];
+});
+
+$factory->state(Image::class, 'user', function (Faker $faker) {
+
+    $fineName = $faker->numberBetween(1, 6) . '.jpg';
+
+    return [
+        
+        'path' => public_path("img/users/{$filesName}"),
+
     ];
 });
